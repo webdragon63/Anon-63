236d4f45129e80204ed802bb39800bf848efeaab
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-06-19 19:16:17 +0000

    Update Anon-63.sh

--- Anon-63.sh
+++ Anon-63.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+#Checking and Installing Required Packages
+pkgs=(figlet lolcat)
+for pkg in ${pkgs[@]}
+do
+ sudo apt install $pkg
+done
+sleep 1
 clear
-echo ---------------GIVE YOUR NETWORK INTERFACE IN THE GIVEN SPACE-------------------
-read -p "GIVE YOUR NETWORK INTERFACE i.e (wlp1s0, wlan0, eth0)-->" NET
+
+clear
+figlet -s -c ANON-63 | lolcat
+echo -----------------GIVE YOUR NETWORK INTERFACE IN THE GIVEN SPACE-----------------| lolcat
+sleep 1
+read -p "SET YOUR NETWORK INTERFACE i.e (wlp1s0, wlan0, eth0)-->" NET
+ifconfig $NET down
 macchanger -r $NET
 clear
 ifconfig $NET up
 clear
-echo ----------------------SET YOUR IP IN THE GIVEN SPACE---------------------------
+figlet -s -c ANON-63 | lolcat
+echo ------------------------SET YOUR IP IN THE GIVEN SPACE--------------------------| lolcat
+sleep 1
 read -p "SET YOUR IP-->" IP
+sleep 2
+echo ------------------------YOUR NEW IP HAS BEEN CONFIGURED-------------------------| lolcat
 ifconfig $NET $IP
-echo ----------------------YOUR NEW IP HAS BEEN CONFIGURED--------------------------
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 ifconfig $NET
-echo -----------------YOUR MAC ADDRESS AND IP HAS BEEN CHANGED----------------------
+echo --------------------YOUR MAC ADDRESS AND IP HAS BEEN CHANGED--------------------| lolcat
+echo -----------------------------------HAPPY JOURNEY--------------------------------| lolcat
