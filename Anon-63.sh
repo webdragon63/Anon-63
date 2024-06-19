3277b443a824f1041a2d33f8f0e3fcd6471c69e4
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-06-19 02:11:10 +0000

    Add files via upload

---
+++ Anon-63.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo ---------------SET YOUR NETWORK INTERFACE IN THE GIVEN SPACE-------------------
+read -p "SET YOUR NETWORK INTERFACE-->" NET
+ifconfig $NET down
+macchanger -r $NET
+clear
+ifconfig $NET up
+clear
+echo ----------------------SET YOUR IP IN THE GIVEN SPACE---------------------------
+read -p "SET YOUR IP-->" IP
+ifconfig $NET $IP
+echo ----------------------YOUR NEW IP HAS BEEN CONFIGURED--------------------------
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+ifconfig $NET
+echo -----------------YOUR MAC ADDRESS AND IP HAS BEEN CHANGED----------------------
